c35edc80f8cd36bf3f8fc52b86c43171ee2a7905
Ilia Sergachev <1894984+sergachev@users.noreply.github.com>
2022-11-05 13:30:50 +0100

    up model url

--- get_mem_model.sh
+++ get_mem_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -nc https://www.micron.com/~/media/documents/products/sim-model/nor-flash/serial/bfm/n25q/n25q128a13e_3v_micronxip_vg12,-d-,tar.gz
+wget -nc -O n25q128a13e_3v_micronxip_vg12,-d-,tar.gz https://media-www.micron.com/-/media/client/global/documents/products/sim-model/nor-flash/serial/bfm/n25q/n25q128a13e_3v_micronxip_vg12,-d-,tar.gz?rev=0aafb8ea0b03403084d8967562251fd9
 md5sum -c checksums.txt
 tar xf n25q128a13e_3v_micronxip_vg12,-d-,tar.gz
 patch N25Q128A13E_VG12/code/N25Qxxx.v n25q_force.patch
